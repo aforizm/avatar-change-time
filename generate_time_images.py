e37094919463bce0ce8330ffbce5150b4d3d49b8
Ilya <ilya@myhost>
2019-06-23 15:18:01 +0300

    add text Moscow time

--- generate_time_images.py
+++ generate_time_images.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 FONT_SIZE = 50
 TEXT_Y_POSITION = 1
 TEXT_X_POSITION = 1
-MOSCOW_UTC = 3
+MOSCOW_UTC = 3 #for servers heroku
 
 def convert_time_to_string(dt):
     dt += timedelta(hours=MOSCOW_UTC)
@@ -13,13 +13,15 @@ def convert_time_to_string(dt):
 
 def change_img():
     start_time = datetime.now()
-    start_time += timedelta(hours=MOSCOW_UTC)
     text = convert_time_to_string(start_time)
     row = Image.new('RGBA', (200, 200), "white")
     parsed = ImageDraw.Draw(row)
     font = ImageFont.truetype("arial.ttf", FONT_SIZE)
+    font2 = ImageFont.truetype("arial.ttf", 18)
     parsed.text((int(row.size[0]*0.2), int(row.size[1]*0.35)), f'{text}', 
                  align="center", font=font, fill=(33,33,212))
+    parsed.text((53, 125),'Moscow time', 
+                 align="center", font=font2, fill=(33,33,212))
     row.save(f'img/time.png', "PNG")
 
 if __name__ == '__main__':
